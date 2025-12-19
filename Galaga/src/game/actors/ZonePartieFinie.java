98f5acdc538c218cfc116fdda2b673104b2a7a62
AiraBira <162131407+AiraBira@users.noreply.github.com>
2025-12-19 16:26:41 +0100

    Update rajout disparition ennemis + score + background + affichage vies + écran fin partie

--- Galaga/src/game/actors/ZonePartieFinie.java
+++ Galaga/src/game/actors/ZonePartieFinie.java
@@ -2,17 +2,12 @@ package game.actors;
 
 import engine.StdDraw;
 
-public class ZoneInfo {
-    
+public class ZonePartieFinie {
     protected int niveau;
-    private static final double LONGUEUR = 0.09;
+    private static final double LONGUEUR = 1;
     private static final double LARGEUR = 1;
     private static final double POSX = 0.0;
     private static final double POSY = 0.0;
-
-    public ZoneInfo(int niveau) {
-        this.niveau = niveau;
-    }
 
     public int getNiveau() {
         return niveau;
@@ -38,14 +33,8 @@ public class ZoneInfo {
         return POSY;
     }
 
-    public void draw(int hp) {
-        StdDraw.setPenColor(StdDraw.WHITE);
-        StdDraw.line(getPosx(), getPosy() + getLongueur(), getPosx()+getLargeur(), getPosy()+getLongueur());
-        double gap=0.05;
-        for (int i=1;i<=hp;i++){
-            StdDraw.setPenColor(StdDraw.WHITE);
-            StdDraw.filledSquare(getPosx()+gap, getPosy()+0.05, 0.02);
-            gap+=0.05;
-        }
+    public void draw() {
+        StdDraw.setPenColor(StdDraw.GREEN);
+        StdDraw.filledRectangle(getPosx(), getPosy(), getLongueur(), getLargeur());
     } 
 }
---
+++ galaga_project-main/Galaga/src/game/actors/Mouvements.java
@@ -0,0 +1,120 @@
+package game.actors;
+
+public abstract class Mouvements {
+
+    double posX;
+    double posY;
+    double length;
+    double vitesse;
+    double longueur;
+    double largeur;
+
+    public Mouvements(double posX, double posY, double vitesse, double length) {
+        this.posX = posX;
+        this.posY = posY;
+        this.vitesse = vitesse;
+        this.length = length;
+    }
+
+    public Mouvements(double posX, double posY, double vitesse, double longueur, double largeur) {
+        this.posX = posX;
+        this.posY = posY;
+        this.vitesse = vitesse;
+        this.longueur = longueur;
+        this.largeur = largeur;
+    }
+
+    public double getPosX() {
+        return posX;
+    }
+
+    public void setPosX(double posX) {
+        this.posX = posX;
+    }
+
+    public double getPosY() {
+        return posY;
+    }
+
+    public void setPosY(double posY) {
+        this.posY = posY;
+    }
+
+    public double getVitesse() {
+        return vitesse;
+    }
+
+    public void setVitesse(double vitesse) {
+        this.vitesse = vitesse;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getLongueur() {
+        return longueur;
+    }
+
+    public double getLargeur() {
+        return largeur;
+    }
+    
+    public void mouvementDroit() {
+        setPosX(getPosX() + getVitesse());
+
+        if (getPosX()-(getLength()/2) <= 0.0) { // Ne peuvent pas dépasser les limites.
+            setPosX(0+(getLength()/2));
+        } else if (getPosX()+(getLength()/2) >= 1.0) {
+            setPosX(1.0-(getLength()/2));
+        }
+
+    }
+
+    public void mouvementGauche() {
+        setPosX(getPosX() - getVitesse());
+
+        if (getPosX()-(getLength()/2) <= 0.0) {
+            setPosX(0+(getLength()/2));
+        } else if (getPosX()+(getLength()/2) >= 1.0) {
+            setPosX(1.0-(getLength()/2));
+        }
+
+    }
+
+    public void mouvementHaut() {
+        setPosY(getPosY() + getVitesse());
+
+        if (getPosY()-(getLength()/2) <= 0.0) {
+            setPosY(0+(getLength()/2));
+        } else if (getPosY()+(getLength()/2)>= 1.0) {
+            setPosY(1.0-(getLength()/2));
+        }
+    }
+
+    public void mouvementBas() {
+        setPosY(getPosY() - getVitesse());
+
+        if (getPosY()-(getLength()/2) <= 0.0) {
+            setPosY(0+(getLength()/2));
+        } else if (getPosY()+(getLength()/2) >= 1.0) {
+            setPosY(1.0-(getLength()/2));
+        }
+    }
+
+    public void mouvementMoth() {
+    }
+
+    public void mouvementButterfly() {
+    }
+
+    public void mouvementBee() {
+    }
+
+   
+
+}
